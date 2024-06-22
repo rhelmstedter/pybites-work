d5c5b41eeab92e4e6c4e71074ae78d8d4f68aa04
Russell Helmstedter <rhelmstedter@gmail.com>
2024-06-22 12:30:47 -0700

    newbie 26

---
+++ tutorials/errors.py
@@ -0,0 +1,18 @@
+
+
+# SyntaxError: This error occurs when the code is not syntactically correct.
+print("Hello, world!")
+
+# TypeError: This error occurs when an operation or function is applied to an object of an inappropriate type.
+# print(5 + "five")
+
+# NameError: This error occurs when a local or global name is not found.
+# print(welcome)
+
+# IndexError: This error occurs when you try to access an index that is out of range for a list.
+# nums = [1, 2, 3]
+# print(nums[5])
+
+# KeyError: This error occurs when you try to access a key that is not present in a dictionary.
+# person = {"name": "Alice"}
+# print(person["age"])
