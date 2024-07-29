72d11ae67b3636da1e8d81a15a5b8d5c33c69a99
Russell Helmstedter <rhelmstedter@gmail.com>
2024-07-29 08:53:55 -0700

    added through newbie 41

--- tutorials/classes.py
+++ tutorials/classes.py
@@ -1,16 +1,17 @@
 class Circle:
+    def __str__(self):
+        return f"A circle with a radius of {self.radius}."
+
     def __init__(self, radius):
         self.radius = radius
 
     def calc_area(self):
-        return 3.14159 * self.radius**2  # Area calculation for the circle.
+        return 3.14159 * self.radius**2
 
     def calc_circumference(self):
-        return 2 * 3.14159 * self.radius  # Circumference calculation for the circle.
+        return 2 * 3.14159 * self.radius
 
 
-my_circle = Circle(3)
+my_circle = Circle(5)
 
-print(f"My circle has a radius of {my_circle.radius}.")
-print(f"My circle has an area of {my_circle.calc_area()}.")
-print(f"My circle has a circumference of {my_circle.calc_circumference()}.")
+print(my_circle)
